aeea064e40bb17e0fa6b464fd0ac3b6c45e63220
iko2000 <shengelia1800@gmail.com>
2025-05-05 11:20:43 +0200

    notifiation

--- routes/file-reciever-route/file-route.js
+++ routes/file-reciever-route/file-route.js
@@ -7,7 +7,6 @@ const path = require('path');
 const axios = require('axios');
 require('dotenv').config();
 const { Resend } = require('resend'); // Import Resend
-const aldbimage = require('../../assets/aldbimage.png');
 
 // Initialize Resend with your API key
 
@@ -194,7 +193,7 @@ async function sendAnalysisEmail(filename, analysis, pdfBuffer, externalrecipien
           <body>
             <div class="container">
               <div class="header">
-                <img src="${aldbimage}" alt="ALDB Associates - Environment Health & Safety" />
+                <img src="../../assets/aldbimage.png" alt="ALDB Associates - Environment Health & Safety" />
               </div>
               <h3>Please find attached the Health & Safety Inspection Report related to the ongoing project referenced above. This report has been compiled in accordance with the scope of services agreed upon with the commissioning client and reflects the site conditions observed during the time of the inspection.</h3>
               <p>Recipients should be: ${externalrecipients[0]} </p>
